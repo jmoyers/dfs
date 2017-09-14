c1eeabf1b08a2353a30d15b9028e7ed4eb0591c9
Joshua Moyers <josh@jmoyers.org>
2017-09-13 20:03:00 -0700

    Better name for bfs visit queue

--- Sources/bfs.swift
+++ Sources/bfs.swift
@@ -2,20 +2,20 @@ class BFS : GraphSearch {
   // An interesting side effect is that naturally pathBack will contain
   // the shortest path available to get from one node to another
   @discardableResult func bfs(start v: Int = 0) -> [Int] {
-    var q = [v]
+    var visitQueue = [v]
     visited[v] = true
 
     var neighbors = [Int]()
 
-    while (q.count > 0) {
-      let next = q[0]
-      q.remove(at:0)
+    while (visitQueue.count > 0) {
+      let next = visitQueue[0]
+      visitQueue.remove(at:0)
 
       print("Search \(next)")
 
       for w in graph.adjacent(next) ?? [] {
         if visited[w] == false {
-          q.append(w)
+          visitQueue.append(w)
           visited[w] = true
           pathBack[w] = next
           neighbors.append(w)
